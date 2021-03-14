76ee79ad1b76e9bfb3d00defcbe3026153bbe3bd
GeorgeTyupin <georgytyupin@yandex.ru>
2021-03-14 16:44:20 +0300

    14.03.2021
    Добавил создание объявления из формы

--- static/js/script.js
+++ static/js/script.js
@@ -7,7 +7,23 @@ function createForm() {
 	btn = document.querySelector(".create-ad");
 	btn.addEventListener('click', function(){
 		document.querySelector('.background-form').classList.remove('hide');
-	})
+	});
+
+}
+
+function closeForm() {
+	document.querySelector('.background-form').classList.add('hide');
+};
+
+function createAd() {
+	$('.content').append(`				
+				<div class="ad" class="ad">
+					<div class="ad-img"></div>
+					<h2 class="ad-name">${document.querySelector('.form-input-name').value}</h2>
+					<p class="ad-category">Категория:</p>
+					<a href="#" class="categories">${document.querySelector('.form-input-category').value}</a>
+				</div>`)
+	console.log("hello")
 }
 
 function main() {
