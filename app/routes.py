e5da05133ffd401a5fa20a5d168cb268521131be
GeorgeTyupin <georgytyupin@yandex.ru>
2021-04-15 19:56:50 +0300

    15.04.2021

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 from flask import Flask , render_template , make_response , request , session , redirect
 import core
+from app import app
 
 db = core.database.Database()
 print(db.test())
 
-app = Flask(__name__)
 app.config['SECRET_KEY'] = "f116d0a5491cbe27e7bb07016b694eb4f6a1976e9f9c55621b9c5418567ac02c"
 
 @app.route("/", methods=['GET', 'POST'])
@@ -43,5 +43,3 @@ def reg():
         login = request.form.get('user_name')
         password = request.form.get('user_password')
         return core.reg.POST(session, login, str(password))
-
-app.run(debug=True)
