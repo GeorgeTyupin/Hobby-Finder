9758aea8ab92619063794f08ba963a418539c3a4
GeorgeTyupin <georgytyupin@yandex.ru>
2021-04-15 20:14:00 +0300

    15.04.2021

--- app/core/database.py
+++ app/core/database.py
@@ -7,19 +7,19 @@ class Database():
         # self.data = data
 
     def loadUsersTable(self, login):
-        with sqlite3.connect('database/database.db') as cur:
+        with sqlite3.connect(DATA_DST) as cur:
             sql = f"SELECT * FROM users WHERE user_name = '{login}' "
             result = cur.execute(sql).fetchone()
             return result
     
     def loadAdsTable(self):
-        with sqlite3.connect('database/database.db') as cur:
+        with sqlite3.connect(DATA_DST) as cur:
             sql = f"SELECT * FROM ads "
             result = cur.execute(sql).fetchall()
             return result
     
     def checkingAdForUniqueness(self, data):
-        with sqlite3.connect('database/database.db') as cur:
+        with sqlite3.connect(DATA_DST) as cur:
             sql = f"SELECT * FROM ads WHERE ad_name = '{data['ad_name']}' AND author_id = '{data['author_id']}' "
             result = cur.execute(sql).fetchall()
             print(result)
@@ -27,14 +27,14 @@ class Database():
                 return '123'
 
     def addUser(self, login, password):
-        with sqlite3.connect("database/database.db") as cur:
+        with sqlite3.connect(DATA_DST) as cur:
             sql = f"""INSERT INTO users ('user_name' , 'password') 
                 VALUES ('{login}','{password}')"""
             cur.execute(sql)
             cur.commit()
 
     def addAD(self, data):
-        with sqlite3.connect("database/database.db") as cur:
+        with sqlite3.connect(DATA_DST) as cur:
             sql = f"""INSERT INTO ads ('ad_name' , 'author_id', 'ad_category', 'ad_description') 
                 VALUES ('{data['ad_name']}','{data['author_id']}','{data['ad_category']}','{data['ad-description']}')"""
             cur.execute(sql)
@@ -42,7 +42,7 @@ class Database():
 
     def test(self):
         try:
-            with sqlite3.connect('database/database.db') as cur:
+            with sqlite3.connect(DATA_DST) as cur:
                 sql = f"SELECT * FROM users "
                 result = cur.execute(sql).fetchone()
                 return result, sql, "База данных без ошибки:", DATA_DST
